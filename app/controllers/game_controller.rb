c9c885a5ba6d56c336e49e7ba5b3a9a895f5a022
JohanPer <johan.perrusson@hotmail.fr>
2022-12-18 20:12:58 +0100

    remove @

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,11 +1,11 @@
 class GameController < ApplicationController
   def new_party
     @party = Party.new
-    @voeylles = %w[A E I O U]
-    @consonnes = %w[B C D F G H J K L M N P Q R S T V W X Y Z]
-    @voeylles = @voeylles.sample(5)
-    @consonnes = @consonnes.sample(5)
-    @party.ten_letters_list = @voeylles + @consonnes
+    voeylles = %w[A E I O U]
+    consonnes = %w[B C D F G H J K L M N P Q R S T V W X Y Z]
+    voeylles = voeylles.sample(5)
+    consonnes = consonnes.sample(5)
+    @party.ten_letters_list = voeylles + consonnes
     @party.save
   end
 
